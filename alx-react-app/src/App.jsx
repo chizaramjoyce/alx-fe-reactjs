711389bc8ab206ea94c3152969f2b4c6b68be720
Uzogo Onyinyechi <chizaramjoyce8@gmail.com>
2025-07-20 22:19:25 +0100

    use jsx with props in component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
     <>
       <Header />
+      <UserProfile name="Uzogo Onyinyechi" age={25} bio="Loves yoghurt drink" />
       <MainContent />
       <Footer />
     </>
