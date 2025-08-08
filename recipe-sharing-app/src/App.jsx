bd0b94779195a054265223eca04c9bb77fc0c4ac
Uzogo Onyinyechi <chizaramjoyce8@gmail.com>
2025-08-08 22:07:09 +0100

    set up recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AddRecipeForm from './components/RecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
