f3e6da80eff861d01eaeeb813733e0d42db2f559
Uzogo Onyinyechi <chizaramjoyce8@gmail.com>
2025-08-14 21:16:59 +0100

    make advance intereactivity

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg" >
-      <img className="rounded-full sm:w-24 md:w-36 sm:h-24 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4" >John Doe</h1>
+    <div className="user-profile hover:shadow-xl bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg" >
+      <img className="hover:scale-110 transition-transform duration-300 ease-in-out rounded-full sm:w-24 md:w-36 sm:h-24 md:h-36 mx-auto" src="https://placehold.co/400" alt="User" />
+      <h1 className="hover:text-blue-500 sm:text-lg md:text-xl text-blue-800 my-4" >John Doe</h1>
       <p className="text-gray-600 sm:text-sm md:text-base" >Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
