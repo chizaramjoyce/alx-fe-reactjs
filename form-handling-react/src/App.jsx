10397c56c51cdaa9633813067fcf6fa6e1ae6ddb
Uzogo Onyinyechi <chizaramjoyce8@gmail.com>
2025-08-22 19:25:06 +0100

    change file name

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import FormikForm from './components/FormikForm'
+import FormikForm from './components/formikForm'
 import RegistrationForm from './components/RegistrationForm'
 
 function App() {
